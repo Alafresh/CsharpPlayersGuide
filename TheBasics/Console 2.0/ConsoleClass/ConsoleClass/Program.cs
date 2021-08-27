c93daa0353dbe22e39d60f25a1cb237d81bfb198
Alafresh <alafresh16@gmail.com>
2021-08-27 13:18:14 -0500

    Some tricks to make working with text and console window easier

---
+++ TheBasics/Console 2.0/ConsoleClass/ConsoleClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello world!";
+            Console.Beep(440, 1000);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Press any key when you're ready to begin. ");
+            Console.ReadKey(true);
+            Console.WriteLine("Hola\nMundo");
+            Console.WriteLine("C:\\Users\\RB\\Desktop\\MyFile.txt");
+            Console.WriteLine(@"C:\Users\RB\Desktop\MyFile.txt");
+            int favoriteNumber = 4;
+            Console.WriteLine($"My favorite number is {favoriteNumber}.");
+            Console.WriteLine($"{Math.PI:0.000}");
+        }
+    }
+}
