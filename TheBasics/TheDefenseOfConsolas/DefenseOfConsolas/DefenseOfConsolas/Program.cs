dc7ff0e9824f9ffa39584b5183fd1b6af215bdb5
Alafresh <alafresh16@gmail.com>
2021-08-27 14:10:52 -0500

    one possible solution to this challenge

---
+++ TheBasics/TheDefenseOfConsolas/DefenseOfConsolas/DefenseOfConsolas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefenseOfConsolas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Defense of Consolas";
+            int row;
+            int column;
+            Console.Write("Target Row ? ");
+            row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Target Column ? ");
+            column = Convert.ToInt32(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Deploy to:\n" +
+                $"({row}, {column - 1})\n" +
+                $"({row - 1}, {column})\n" +
+                $"({row}, {column + 1})\n" +
+                $"({row + 1}, {column})\n");
+            Console.Beep(200, 2000);
+        }
+    }
+}
