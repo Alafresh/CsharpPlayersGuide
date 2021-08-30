c0e8ac9a9f5f4bb757d38d329dd39d0123f44303
Alafresh <alafresh16@gmail.com>
2021-08-30 11:17:29 -0500

    Challenge Repairing The Clocktower

---
+++ TheBasics/ClockTower/RepairingTheClocktower/RepairingTheClocktower/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RepairingTheClocktower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine("Tick");
+            else
+                Console.WriteLine("Tock");
+        }
+    }
+}
