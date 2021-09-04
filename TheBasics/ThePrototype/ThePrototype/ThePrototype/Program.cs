53cc54c2039ba76c9b0955a55befeb49d8457b98
Alafresh <alafresh16@gmail.com>
2021-09-04 14:52:53 -0500

    one possible solution to this challenge

---
+++ TheBasics/ThePrototype/ThePrototype/ThePrototype/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ThePrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("User 1, enter a number between 0 and 100: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0 || num > 100)
+                    continue;
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("User 2, guess the number.");
+                    while (true)
+                    {
+                        Console.Write("What is your next guess? ");
+                        int guess = Convert.ToInt32(Console.ReadLine());
+                        if (num == guess)
+                        {
+                            Console.WriteLine("You guessed the number!");
+                            break;
+                        }
+                        else if (guess < num)
+                            Console.WriteLine($"{guess} is too low.");
+                        else
+                            Console.WriteLine($"{guess} is too high");
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
