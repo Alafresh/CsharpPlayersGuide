52fb87f91f4cdaa9fa96b36d35f0e091e41cf361
Alafresh <alafresh16@gmail.com>
2021-09-04 14:24:54 -0500

    Level 11 Looping

---
+++ TheBasics/BreakAndContinue/BreakOutOfLoops/BreakOutOfLoops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BreakOutOfLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Think of a number and type it here: ");
+                string input = Console.ReadLine();
+
+                if (input == "quit" || input == "exit")
+                    break;
+
+                int number = Convert.ToInt32(input);
+
+                if (number == 12)
+                {
+                    Console.WriteLine("I dont like that number. Pick another one");
+                    continue;
+                }
+                Console.WriteLine($"I like {number}. Its the one before {number + 1}");
+            }
+            int totalRows = 5;
+            int totalColumns = 10;
+
+            for (int currentRow = 1; currentRow <= totalRows; currentRow++)
+            {
+                for (int currentColumn = 1; currentColumn <= totalColumns; currentColumn++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
