b1156028e718710404c6399f752b2e5e390656d7
Alafresh <alafresh16@gmail.com>
2021-09-08 10:33:20 -0500

    features Add challenge rework existing loop

---
+++ TheBasics/TheLawsOfFreach/TheLawsOfFreach/TheLawsOfFreach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheLawsOfFreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            int currentMinimum = int.MaxValue;
+
+            foreach(int item in array)
+            {
+                if (item < currentMinimum)
+                    currentMinimum = item;
+            }
+            Console.WriteLine(currentMinimum);
+            int total = 0;
+            foreach(int item in array)
+            {
+                total += item;
+            }
+            float average = (float)total / array.Length;
+            Console.WriteLine(average);
+        }
+    }
+}
