cd2eefd1cbde9841401d5b5065b9eafeb5476ab9
Alafresh <alafresh16@gmail.com>
2021-09-09 13:59:25 -0500

    Main Add basics of recursion

---
+++ TheBasics/CountDown/CountDown/CountDown/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CountDown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountDown(10);
+            void CountDown(int number)
+            {
+                Console.WriteLine(number);
+                if (number > 0)
+                    CountDown(number - 1);
+                    
+            }
+        }
+    }
+}
